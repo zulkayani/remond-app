b921eb929bcd7788472a66faa9ea16e372320865
Muhammad Zulkayani <m.ulkayani@gmail.com>
2020-07-27 15:08:40 +0700

    Bab 11 done

--- config/db.js
+++ config/db.js
@@ -1,20 +1,20 @@
-const mongoose = require ('mongoose');
-const config = require ('config');
-const db = config.get ('mongoURI');
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
 
 // koneksi db
 const connectDB = async () => {
-    try {
-      await mongoose.connect(db, { useNewUrlParser: true });
-  
-      console.log('MongoDB Connected...')
-    } catch (err) {
-      console.error(err.message);
-  
-      // exit process with failure
-      process.exit(1);
-    }
+  try {
+    await mongoose.connect(db, { useNewUrlParser: true, useCreateIndex: true });
+
+    console.log('MongoDB Connected...')
+  } catch (err) {
+    console.error(err.message);
+
+    // exit process with failure
+    process.exit(1);
   }
-  
-  // exports module
-  module.exports = connectDB; 
+}
+
+// exports module
+module.exports = connectDB;
